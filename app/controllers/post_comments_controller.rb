61ebddad5c16df5b2e6955101435e5a7ace571ba
mwataru-pro <m1130wata@gmail.com>
2020-04-29 15:36:35 +0900

    [Start] 5-4

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,6 @@
+class PostCommentsController < ApplicationController
+ 
+ def create
+ end
+
+end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,4 +4,6 @@ class PostImage < ApplicationRecord
 
 	attachment :image
 
+	has_many :post_comments, dependent: :destroy
+
 end
